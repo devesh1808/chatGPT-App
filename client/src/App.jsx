271c23b215a2e9701a5b17a2bc165de9cb5210cd
devesh1808 <devesh1808@gmail.com>
2023-11-22 17:45:18 +0530

    imported images, main and footer section in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
+import send from "./assets/send.svg";
+import user from "./assets/user.png";
+import loader from "./assets/loader.svg";
 
 function App() {
-
   return (
-    <div className="App">
-      App
-    </div>
-  )
+    <main className="chatGPT-app">
+      <section className="chat-container">
+        <div className="layout"></div>
+      </section>
+      <footer>
+        <input
+          className="composebar"
+          autoFocus
+          type="text"
+          placeholder="Ask me anything!"
+          onChange={() => {}}
+        />
+        <div className="send-button">
+          <img src={send} />
+        </div>
+      </footer>
+    </main>
+  );
 }
 
-export default App
+export default App;
